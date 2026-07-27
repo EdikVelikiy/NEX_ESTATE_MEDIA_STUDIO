acd91c25dc638ccbacc226803a793b89e3b4493a
EdikVelikiy <ba1ance1331@gmail.com>
2026-07-27 03:51:46 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'nex-estate-media-studio-v1-5-corporate';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png',
+  './assets/icons/icon-maskable-192.png',
+  './assets/icons/icon-maskable-512.png',
+  './assets/marks/A_dark.svg',
+  './assets/marks/A_light.svg',
+  './assets/marks/B_dark.svg',
+  './assets/marks/B_light.svg',
+  './assets/marks/C_dark.svg',
+  './assets/marks/C_light.svg',
+  './assets/marks/D_dark.svg',
+  './assets/marks/D_light.svg',
+  './assets/marks/E_dark.png',
+  './assets/marks/E_light.png',
+  './assets/marks/F_dark.svg',
+  './assets/marks/F_light.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+        return response;
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then(cached => cached || fetch(request).then(response => {
+      if (response && response.ok && response.type === 'basic') {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+      }
+      return response;
+    }))
+  );
+});
